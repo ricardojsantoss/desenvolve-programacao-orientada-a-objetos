9b923f0a0cbc1c6e5fbb2d1648253fe01d6377fb
ricardojsantoss <ricardojsantos@outlook.com.br>
2026-09-07 14:43:56 -0300

    Add files via upload

---
+++ Módulo II/projeto_biblioteca/Pessoa.java
@@ -0,0 +1,17 @@
+package projeto_biblioteca;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ Módulo II/projeto_biblioteca/Usuario.java
@@ -0,0 +1,18 @@
+package projeto_biblioteca;
+
+public class Usuario extends Pessoa {
+    private int idade;
+
+    public Usuario(String nome, int idade) {
+        super(nome);
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
